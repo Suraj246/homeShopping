83f7034bd17a7fc77301694a66c4c3e86343b666
suraj <surajsurwase9881@gmail.com>
2025-07-18 17:20:12 +0530

    api endpoing changed

--- src/API_ENDPOINT.jsx
+++ src/API_ENDPOINT.jsx
@@ -1,2 +1,2 @@
-// export const apiEndpoint = 'https://shopping-cart-backend-cqtv.onrender.com'
-export const apiEndpoint = 'http://localhost:5000'
+export const apiEndpoint = 'https://shopping-cart-backend-cqtv.onrender.com'
+// export const apiEndpoint = 'http://localhost:5000'
